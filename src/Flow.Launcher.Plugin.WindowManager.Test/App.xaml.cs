9d7e370c9b89dce0fd6deeff1c8536144d885463
Jack251970 <1160210343@qq.com>
2025-07-07 14:17:16 +0800

    Improve code quality

--- src/Flow.Launcher.Plugin.WindowManager.Test/App.xaml.cs
+++ src/Flow.Launcher.Plugin.WindowManager.Test/App.xaml.cs
@@ -1,14 +1,7 @@
-﻿using System.Configuration;
-using System.Data;
-using System.Windows;
+﻿using System.Windows;
 
-namespace Flow.Launcher.Plugin.WindowManager.Test
+namespace Flow.Launcher.Plugin.WindowManager.Test;
+
+public partial class App : Application
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
-    {
-    }
-
 }
